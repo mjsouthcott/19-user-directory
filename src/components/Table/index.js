1ab931d64ef8a91cab86177d21e7a3b62888e8cd
Matthew Southcott <mjsouthcott@gmail.com>
2020-06-24 21:12:57 -0400

    Add filtering

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.css"
 import users from '../../users.json'
+const to = require('to-case')
 
 function Table (props) {
-  let sortColumn = ''
-  let sortOrder = ''
+  const [usersSubset, setUsersSubset] = useState(users)
+  let sortColumn, filterColumn = 'id'
+  let sortOrder = 'asc'
+  let filterValue = ''
 
   function setSortColumn (value) {
     sortColumn = value
@@ -17,15 +20,52 @@ function Table (props) {
   }
 
   function sort () {
+    let sortedUsers = []
 
+        // TODO: Add logic to sort users
+
+    setUsersSubset(sortedUsers)
   }
 
   function setFilterColumn (value) {
-
+    filterColumn = value
+    console.log(filterColumn)
   }
 
   function filter () {
+    let filterValueInput = document.getElementById('filter-value')
+    filterValue = to.title(filterValueInput.value)
+    filterValueInput.value = ''
+    let filteredUsers = []
 
+    switch (filterColumn) {
+      case 'rank':
+        filteredUsers = users.filter(user => { return user.rank === filterValue })
+        break
+      case 'firstName':
+        filteredUsers = users.filter(user => { return user.first_name === filterValue })
+        break
+      case 'lastName':
+        filteredUsers = users.filter(user => { return user.last_name === filterValue })
+        break
+      case 'occupation':
+        filteredUsers = users.filter(user => { return user.occupation === filterValue })
+        break
+      case 'section':
+        filteredUsers = users.filter(user => { return user.section === filterValue })
+        break
+      case 'position':
+        filteredUsers = users.filter(user => { return user.position === filterValue })
+        break
+      case 'yearsOfService':
+        filteredUsers = users.filter(user => { return user.years_of_service === parseInt(filterValue) })
+        break
+      default:
+        filteredUsers = users.filter(user => { return user.id === parseInt(filterValue) })
+        break
+    }
+
+    setUsersSubset(filteredUsers)
   }
 
   return (
@@ -46,13 +86,13 @@ function Table (props) {
                   </button>
                   <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <button className="dropdown-item" type="button" onClick={() => setSortColumn('id')}>ID (Default)</button>
-                    <button className="dropdown-item" type="button">Rank</button>
-                    <button className="dropdown-item" type="button">First Name</button>
-                    <button className="dropdown-item" type="button">Last Name</button>
-                    <button className="dropdown-item" type="button">Occupation</button>
-                    <button className="dropdown-item" type="button">Section</button>
-                    <button className="dropdown-item" type="button">Position</button>
-                    <button className="dropdown-item" type="button">Years of Service</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortColumn('rank')}>Rank</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortColumn('firstName')}>First Name</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortColumn('lastName')}>Last Name</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortColumn('occupation')}>Occupation</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortColumn('section')}>Section</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortColumn('position')}>Position</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortColumn('yearsOfService')}>Years of Service</button>
                   </div>
                 </div>
                 <div className="dropdown">
@@ -61,10 +101,10 @@ function Table (props) {
                   </button>
                   <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <button className="dropdown-item" type="button" onClick={() => setSortOrder('asc')}>Ascending (Default)</button>
-                    <button className="dropdown-item" type="button">Desending</button>
+                    <button className="dropdown-item" type="button" onClick={() => setSortOrder('desc')}>Descending</button>
                   </div>
                 </div>
-                <button className="btn btn-secondary" type="button">Sort</button>
+                <button className="btn btn-secondary" type="button" onClick={() => sort()}>Sort</button>
               </div>
             </div>
           </div>
@@ -81,14 +121,14 @@ function Table (props) {
                     Column
                   </button>
                   <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <button className="dropdown-item" type="button">ID (Default)</button>
-                    <button className="dropdown-item" type="button">Rank</button>
-                    <button className="dropdown-item" type="button">First Name</button>
-                    <button className="dropdown-item" type="button">Last Name</button>
-                    <button className="dropdown-item" type="button">Occupation</button>
-                    <button className="dropdown-item" type="button">Section</button>
-                    <button className="dropdown-item" type="button">Position</button>
-                    <button className="dropdown-item" type="button">Years of Service</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('id')}>ID (Default)</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('rank')}>Rank</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('firstName')}>First Name</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('lastName')}>Last Name</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('occupation')}>Occupation</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('section')}>Section</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('position')}>Position</button>
+                    <button className="dropdown-item" type="button" onClick={() => setFilterColumn('yearsOfService')}>Years of Service</button>
                   </div>
                 </div>
                 <form className="form-inline">
@@ -96,7 +136,7 @@ function Table (props) {
                     <input type="text" className="form-control" id="filter-value" aria-describedby="filter-value" placeholder="Enter value" />
                   </div>
                 </form>
-                <button className="btn btn-secondary" type="button">Filter</button>
+                <button className="btn btn-secondary" type="button" onClick={() => filter()}>Filter</button>
               </div>
             </div>
           </div>
@@ -118,7 +158,7 @@ function Table (props) {
             </tr>
           </thead>
           <tbody>
-            {users.map(user => (
+            {usersSubset.map(user => (
               <tr>
                 <th scope="row">{user.id}</th>
                 <td><img src={user.rank_badge} alt={user.rank} height="100"></img></td>
